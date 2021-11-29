956a75d36cf4c9a52d17992d081aa73a6f0bf4af
Ankit Lute <ankit.lute@tesco.com>
2021-11-29 19:41:00 +0530

    Refactored user-id route

--- basics/basics-final/lib/user-data.js
+++ basics/basics-final/lib/user-data.js
@@ -3,10 +3,9 @@ import path from 'path'
 
 export function getUserData(userId) {
   const userDataDirectory = path.join(process.cwd(), 'data')
-  const fileContents = fs.readFileSync(path.join(userDataDirectory, 'user-data.json'), 'utf8');
-  const key = `user-${userId}`;
-  const fileContentsObj = JSON.parse(fileContents);
-  const userData = fileContentsObj[key];
+  const allUsersData = JSON.parse(fs.readFileSync(path.join(userDataDirectory, 'user-data.json'), 'utf8'));
+  const userKey = `user-${userId}`;
+  const userData = allUsersData[userKey];
 
   return userData;
 }
--- basics/basics-final/pages/[user-id].js
+++ basics/basics-final/pages/[user-id].js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import utilStyles from '../styles/utils.module.css'
 import { getUserData } from '../lib/user-data'
 
 export default function Home({ userData }) {
@@ -7,6 +8,7 @@ export default function Home({ userData }) {
       <Head>
         <title>{'SSR Page'}</title>
       </Head>
+      <h2 className={utilStyles.headingLg}>Blog</h2>
       <p>{`User Name : ${userData.name}`}</p>
       <p>{`User Address : ${userData.address}`}</p>
     </>
